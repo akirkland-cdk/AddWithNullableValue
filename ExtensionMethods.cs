2da3234ce2c4cac1c59aa1f20eaf2fdd0732aef4
Andy Kirkland <53475954+akirkland-cdk@users.noreply.github.com>
2019-08-15 08:10:03 -0400

    Update ExtensionMethods.cs

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -7,7 +7,7 @@ namespace ExtensionMethods
   {
     public static SqlParameter AddWithNullableValue(this SqlParameterCollection collection, string parameterName,object value)
     {
-      if(value++null)
+      if(value == null)
         return collection.AddWithValue(parameterName, DBNull.Value);
       else
         return collection.AddWithValue(parameterName, value);
